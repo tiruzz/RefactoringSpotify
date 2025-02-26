6851dd0015d8ce775300dc1cebf24a19387756fa
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2025-02-26 10:08:49 +0000

    modifica visulizzazione playlist pubblica

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, redirect, request, url_for, session, render_template
 import spotipy
-from services.spotify_oauth import sp_oauth, get_spotify_object
+from services.spotify_oauth import get_spotify_object, sp_oauth, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+from spotipy.oauth2 import SpotifyClientCredentials
+
 home_bp = Blueprint('home', __name__) 
 
 @home_bp.route('/home')
@@ -24,10 +26,28 @@ def home_page():
 @home_bp.route('/playlist/<playlist_id>')
 def playlist_details(playlist_id):
     token_info = session.get('token_info', None)
-    if not token_info:
-        return redirect(url_for('auth.login'))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    brani = sp.playlist_items(playlist_id)
-    brani_specifici = brani['items']
+    
+    try:
+        if token_info:
+            sp = get_spotify_object(token_info)
+        else:
+            client_credentials_manager = SpotifyClientCredentials(
+                client_id=SPOTIFY_CLIENT_ID,
+                client_secret=SPOTIFY_CLIENT_SECRET
+            )
+            sp = spotipy.Spotify(auth_manager=client_credentials_manager)
+        
+        brani = sp.playlist_items(playlist_id)
+        brani_specifici = brani.get('items', []) if brani and isinstance(brani, dict) else []
+    except spotipy.exceptions.SpotifyException as e:
+        print(f"Errore Spotify: {e}")
+        return "Playlist non trovata o accesso negato", 404
+    
     return render_template('base.html', brani=brani_specifici)
+
+
+
+@home_bp.route('/artist/<artist_id>')
+def artist_details(artist_id):
+
+    return render_template('artista.html', biografia=biografia)
--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -9,7 +9,7 @@ sp_oauth = SpotifyOAuth(
 client_id=SPOTIFY_CLIENT_ID,
 client_secret=SPOTIFY_CLIENT_SECRET,
 redirect_uri=SPOTIFY_REDIRECT_URI,
-scope="user-read-private",
+scope="user-read-private playlist-read-private",
 show_dialog=True 
 )
 
