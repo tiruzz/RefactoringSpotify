cd19466248687e3b4e8cf90f16da1539a1b26311
astorelloo astorelloo <lil.astor06@gmail.com>
2025-03-18 08:43:20 +0000

    entrambe le tabelle sono complete

--- models.py
+++ models.py
@@ -7,3 +7,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
+#commit di amine
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref="playlist", lazy=True)
