a943bbb1114fb3382701ec51296998e1a7a92181
astorelloo astorelloo <lil.astor06@gmail.com>
2025-03-18 08:35:16 +0000

    db va

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from blueprints.auth import auth_bp
 from blueprints.home import home_bp
+from models import db, User
+import requests
 #pip install -r requirements.txt
 app = Flask(__name__)
 app.secret_key = 'chiavesessione'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+with app.app_context():
+    db.create_all()
 
 #collego i blueprint all'app per poter accedere alle loro route
 app.register_blueprint(auth_bp)
 app.register_blueprint(home_bp)
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ blueprints/account.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, redirect, request, url_for, session, render_template
+
+accounte_bp = Blueprint('account', __name__)
+
+@accounte_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            return render_template('register.html', error="Questo username è già in uso.")
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', error=None)
+
+@accounte_bp.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('home'))
+        return render_template('login.html', error="Credenziali non valide.")
+    return render_template('login.html', error=None)
